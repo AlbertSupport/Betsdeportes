767ce7e4f6597c945300ad29e0187ff5496f32e6
AlbertSupport <servicio.compu.afernand@gmail.com>
2025-03-03 17:43:09 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        PictureBox1 = New PictureBox()
+        CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.White
+        WebView21.Dock = DockStyle.Fill
+        WebView21.Location = New Point(0, 0)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(800, 450)
+        WebView21.TabIndex = 0
+        WebView21.ZoomFactor = 1R
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox1.BackColor = SystemColors.ActiveCaptionText
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(511, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(223, 60)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(WebView21)
+        Name = "Form1"
+        Text = "BetsDeportes v1.0"
+        CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Private Const WH_MOUSE_LL As Integer = 14
+    Private Const WM_LBUTTONDOWN As Integer = &H201
+    Private currentUrl As String = "https://shop.passowin.net/#/"
+
+    Private Delegate Function LowLevelMouseProc(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+    Private mouseProc As LowLevelMouseProc = AddressOf MouseHookCallback
+    Private hookId As IntPtr = IntPtr.Zero
+
+
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure MSLLHOOKSTRUCT
+        Public pt As Point
+        Public mouseData As UInteger
+        Public flags As UInteger
+        Public time As UInteger
+        Public dwExtraInfo As IntPtr
+    End Structure
+
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetWindowsHookEx(idHook As Integer, lpfn As LowLevelMouseProc, hMod As IntPtr, dwThreadId As UInteger) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function UnhookWindowsHookEx(hhk As IntPtr) As Boolean
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function CallNextHookEx(hhk As IntPtr, nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Shared Function GetModuleHandle(lpModuleName As String) As IntPtr
+    End Function
+
+
+    Private Function MouseHookCallback(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+        If nCode >= 0 AndAlso wParam = CType(WM_LBUTTONDOWN, IntPtr) Then
+            Dim mouseInfo As MSLLHOOKSTRUCT = Marshal.PtrToStructure(Of MSLLHOOKSTRUCT)(lParam)
+            Dim cursorPosition As Point = mouseInfo.pt
+
+
+            If Not Me.Bounds.Contains(cursorPosition) Then
+                Me.Close()
+            End If
+        End If
+
+        Return CallNextHookEx(hookId, nCode, wParam, lParam)
+    End Function
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Icon = New Icon("Icon/icono.ico")
+        Me.ShowInTaskbar = False
+
+        Me.WindowState = FormWindowState.Maximized
+
+        Me.TopMost = False
+
+        Try
+            Await WebView21.EnsureCoreWebView2Async()
+
+            WebView21.CoreWebView2.Navigate(currentUrl)
+
+            AddHandler WebView21.KeyDown, AddressOf WebView21_KeyDown
+            AddHandler WebView21.CoreWebView2.NavigationStarting, AddressOf WebView21_NavigationStarting
+            AddHandler WebView21.CoreWebView2.NavigationCompleted, AddressOf RemoveBackgroundScript
+            WebView21.CoreWebView2.Settings.AreDevToolsEnabled = False
+
+        Catch ex As Exception
+            MessageBox.Show("Error inicializando WebView2: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub WebView21_KeyDown(sender As Object, e As KeyEventArgs)
+
+        If e.KeyCode = Keys.F12 Then
+            e.SuppressKeyPress = True
+            e.Handled = True
+            Return
+        End If
+
+        If e.KeyCode = Keys.F8 Then
+            e.SuppressKeyPress = True
+            e.Handled = True
+        End If
+
+        If e.KeyCode = Keys.F3 Then
+
+            currentUrl = "https://shop.passowin.net/#/config"
+            WebView21.CoreWebView2.Navigate(currentUrl)
+
+        ElseIf e.KeyCode = Keys.F5 Then
+
+            currentUrl = "https://shop.passowin.net/#/"
+            WebView21.CoreWebView2.Navigate(currentUrl)
+
+
+            WebView21.CoreWebView2.Reload()
+        End If
+    End Sub
+
+    Private Sub WebView21_NavigationStarting(sender As Object, e As Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs)
+        If e.Uri <> currentUrl Then
+            e.Cancel = True
+        End If
+    End Sub
+
+
+
+    Private Sub RemoveBackgroundScript(sender As Object, e As Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs)
+        Dim script As String = "
+    (function() {
+        function applyChanges() {
+            let logoElement = document.querySelector('.logo');
+            if (logoElement) {
+                logoElement.style.setProperty('background', 'none', 'important');
+            }
+            
+            let logoElement2 = document.querySelector('.cpnContent');
+            if (logoElement2) {
+                logoElement2.style.setProperty('background', 'none', 'important');
+                logoElement2.style.backgroundColor = '#B6B6BB'; 
+            }
+
+            let logoutButton = document.querySelector('li.logOut');
+            if (logoutButton) {
+                logoutButton.parentNode.removeChild(logoutButton);
+            }
+
+            let checkIcons = document.querySelectorAll('li.checkcppn');
+            if (checkIcons.length > 3 && checkIcons[3].parentNode) {
+                checkIcons[3].parentNode.removeChild(checkIcons[3]);
+            }
+            if (checkIcons[0] && checkIcons[0].parentNode) {
+                checkIcons[0].parentNode.removeChild(checkIcons[0]);
+            }
+
+            let settings = document.querySelector('li.settings');
+            if (settings && settings.parentNode) {
+                settings.parentNode.removeChild(settings);
+            }
+
+            let paises = document.querySelector('li#langOpen');
+            if (paises && paises.parentNode) {
+                paises.parentNode.removeChild(paises);
+            }
+
+            let keyboardShortcut = document.querySelector('span.keyboardshortcut4');
+            if (keyboardShortcut && keyboardShortcut.parentNode) {
+                keyboardShortcut.parentNode.removeChild(keyboardShortcut);
+            }
+
+            let keyboardShortcut2 = document.querySelector('span.keyboardshortcut2');
+            if (keyboardShortcut2 && keyboardShortcut2.parentNode) {
+                keyboardShortcut2.parentNode.removeChild(keyboardShortcut2);
+            }
+
+            let keyboardShortcut3 = document.querySelector('span.keyboardshortcut');
+            if (keyboardShortcut3 && keyboardShortcut3.parentNode) {
+                keyboardShortcut3.parentNode.removeChild(keyboardShortcut3);
+            }
+        }
+
+        // Aplicar cambios inmediatamente
+        applyChanges();
+
+        // Observar cambios en el DOM y aplicar cambios dinámicamente
+        let observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                applyChanges();
+            });
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+    })();
+"
+
+        WebView21.CoreWebView2.ExecuteScriptAsync(script)
+
+
+    End Sub
+
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        If hookId <> IntPtr.Zero Then
+            UnhookWindowsHookEx(hookId)
+        End If
+    End Sub
+
+
+End Class
+
+
